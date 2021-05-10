e724270cc089118459f06588dc0dca12285be8c5
Jeandre <jeandreross@gmail.com>
2021-05-10 08:36:51 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,135 @@
+// Initialize fullpage Fullpage
+new fullpage("#fullpage", {
+    anchors: ["homePage", "aboutMe", "workPage", "skillsPage", "projectsPage", "testimonialPage", "contactPage"],
+    navigation: true,
+    navigationPosition: "right",
+    navigationTooltips: ["Home", "About Me", "Work and Education", "Skills", "Projects", "Testimonials", "Contact"],
+    center: true,
+  }); 
+
+  function toggleNavbar() {
+    document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
+  }
+
+  // TypeWriter Effect
+  var container = document.getElementById('changeText');
+
+var things = ['Passion for UI/UX Design', 'Gamer', 'Sports Fanatic'];
+var t = -1;
+var thing = '';
+var message = container.innerHTML;
+var mode = 'write';
+var delay = 1500;
+
+function updateText(txt) {
+    container.innerHTML = txt;
+}
+
+function tick() {
+
+    if(container.innerHTML.length == 0) {
+        t++;
+        thing = things[t];
+        message = '';
+        mode = 'write';
+    }
+
+    switch(mode) {
+        case 'write' :
+            message += thing.slice(0, 1);
+            thing = thing.substr(1);
+
+            updateText(message);
+
+            if(thing.length === 0 && t === (things.length - 1)) {
+            	window.clearTimeout(timeout);
+            	return;
+            }
+
+            if(thing.length == 0){
+                mode = 'delete';
+                delay = 1500;
+            } else {
+                delay = 32 + Math.round(Math.random() * 40);
+            }
+
+            break;
+
+        case 'delete' :
+            message = message.slice(0, -1);
+            updateText(message);
+
+            if(message.length == 0)
+            {
+                mode = 'write';
+                delay = 1500;
+            } else {
+                delay = 32 + Math.round(Math.random() * 100);
+            }
+            break;
+    }
+
+    timeout = window.setTimeout(tick, delay);
+}
+
+var timeout = window.setTimeout(tick, delay);var container = document.getElementById('changeText');
+
+var things = ['I write code on the Internet...', 'The web is my passion, and I want to make a difference', 'Is this how you add text lol'];
+var t = -1;
+var thing = '';
+var message = container.innerHTML;
+var mode = 'write';
+var delay = 1000;
+
+function updateText(txt) {
+    container.innerHTML = txt;
+}
+
+function tick() {
+
+    if(container.innerHTML.length == 0) {
+        t++;
+        thing = things[t];
+        message = '';
+        mode = 'write';
+    }
+
+    switch(mode) {
+        case 'write' :
+            message += thing.slice(0, 1);
+            thing = thing.substr(1);
+
+            updateText(message);
+
+            if(thing.length === 0 && t === (things.length - 1)) {
+            	window.clearTimeout(timeout);
+            	return;
+            }
+
+            if(thing.length == 0){
+                mode = 'delete';
+                delay = 1500;
+            } else {
+                delay = 32 + Math.round(Math.random() * 40);
+            }
+
+            break;
+
+        case 'delete' :
+            message = message.slice(0, -1);
+            updateText(message);
+
+            if(message.length == 0)
+            {
+                mode = 'write';
+                delay = 1500;
+            } else {
+                delay = 32 + Math.round(Math.random() * 100);
+            }
+            break;
+    }
+
+    timeout = window.setTimeout(tick, delay);
+}
+
+var timeout = window.setTimeout(tick, delay);
